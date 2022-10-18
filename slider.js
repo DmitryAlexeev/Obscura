e30f52bf2d10a75ae1dd7882e9ce88942d926039
DmitryAlexeev <113858938+DmitryAlexeev@users.noreply.github.com>
2022-10-18 22:20:36 +0300

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,34 @@
+const slider = $(".feedback__list").bxSlider({
+    pager: false,
+    controls: false,
+    touchEnabled: true,
+    hideControlOnEnd: false,
+});
+
+$("button--prev").click((e) => {
+    e.preventDefault();
+    slider.goToPrevSlide();
+});
+
+$("button--next").click((e) => {
+    e.preventDefault();
+    slider.goToNextSlide();
+});
+
+const portfolio = $(".portfolio__list").bxSlider({
+    pager: false,
+    controls: false,
+    infiniteLoop: true,
+    touchEnabled: true,
+    hideControlOnEnd: false,
+});
+
+$("button--portfolio--prev").click((e) => {
+    e.preventDefault();
+    portfolio.goToPrevSlide();
+});
+
+$("button--portfolio--next").click((e) => {
+    e.preventDefault();
+    portfolio.goToNextSlide();
+});
